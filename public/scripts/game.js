f6dfdd7baf44839f5da75040299511ada43fe075
hailin chen <hchen256@my.bcit.ca>
2022-05-28 08:51:11 -0700

    Unfinished game logic

--- public/scripts/game.js
+++ public/scripts/game.js
@@ -1,16 +1,15 @@
-let secondCard = undefined;
-let firstCard = undefined;
+let cardOne, cardTwo;
 let cardHasBeenFlipped = false;
-let lockboard = false;
-
-function setTime() {
-  setTimeout(() => {
-    // if all cards flipped win
-    // else lose
-  }, 20000);
-}
+let outOfTime = false;
+let disableDeck = false;
+let matchPair = 0;
+let gridSize = 6;
 
 function shuffle(gridSize) {
+  matchPair = 0;
+  cardOne = cardTwo = "";
+  $(`#${cardOne.id}`).parent().removeClass("flip");
+  $(`#${cardTwo.id}`).parent().removeClass("flip");
   var classList = $("#game_grid .card");
   $.each(classList, function (index, item) {
     let randomNum = Math.floor(Math.random() * gridSize);
@@ -39,33 +38,56 @@ function shuffle(gridSize) {
 //   $("main").html(result);
 // }
 
+function matchCards(cardOne, cardTwo) {
+  if ($(`#${cardOne.id}`).attr("src") === $(`#${cardTwo.id}`).attr("src")) {
+    matchPair++;
+    if (matchPair == gridSize) {
+      window.alert("You win!");
+      setTimeout(() => {
+        shuffle(gridSize);
+      }, 1000);
+    }
+    $(`#${cardOne.id}`).parent().off("click");
+    $(`#${cardTwo.id}`).parent().off("click");
+    return (disableDeck = false);
+  } else {
+    setTimeout(() => {
+      cardOne = cardTwo = "";
+      disableDeck = false;
+    }, 1000);
+  }
+}
+
 function setup() {
-  gridSize = 4;
+  shuffle(gridSize);
   $("#level").change("#level", function () {
-    var gridSize = $(this).val();
-    console.log(gridSize)
+    gridSize = $(this).val();
     shuffle(gridSize);
   });
 
   $(".card").on("click", function () {
-    $(this).toggleClass("flip");
-    if (!cardHasBeenFlipped) {
-      firstCard = $(this).find(".front_face")[0];
-      cardHasBeenFlipped = true;
-    } else {
-      secondCard = $(this).find(".front_face")[0];
-      cardHasBeenFlipped = false;
+    let clickedCard = $(this);
+    console.log(clickedCard);
+    if (clickedCard !== cardOne && !disableDeck && !outOfTime) {
+      clickedCard.toggleClass("flip");
 
-      if (
-        $(`#${firstCard.id}`).attr("src") == $(`#${secondCard.id}`).attr("src")
-      ) {
-        $(`#${firstCard.id}`).parent().off("click");
-        $(`#${secondCard.id}`).parent().off("click");
-      } else {
-        setTimeout(() => {
-          $(`#${firstCard.id}`).parent().removeClass("flip");
-          $(`#${secondCard.id}`).parent().removeClass("flip");
-        }, 1000);
+      if (!cardOne) {
+        return (cardOne = clickedCard);
+      }
+      cardTwo = clickedCard;
+
+      disableDeck = true;
+
+      matchCards(cardOne, cardTwo);
+    } else {
+      if (!outOfTime && !disableDeck) {
+        if (!cardHasBeenFlipped) {
+          firstCard = $(this).find(".front_face")[0];
+          cardHasBeenFlipped = true;
+        } else {
+          secondCard = $(this).find(".front_face")[0];
+          cardHasBeenFlipped = false;
+        }
       }
     }
   });
